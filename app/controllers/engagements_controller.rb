6cc1756635bc65ca677a5290572d466c6d13ae64
James Keane <jpkeane@me.com>
2018-09-20 00:42:53 +0100

    Added Engagement show

--- app/controllers/engagements_controller.rb
+++ app/controllers/engagements_controller.rb
@@ -2,4 +2,8 @@ class EngagementsController < ApplicationController
   def index
     @engagements = Engagement.all
   end
+
+  def show
+    @engagement = Engagement.friendly.find(params[:id])
+  end
 end
--- app/models/engagement.rb
+++ app/models/engagement.rb
@@ -7,6 +7,15 @@ class Engagement < ApplicationRecord
   validates :name, presence: true, length: { maximum: 100 }
   validates :code, presence: true, length: { maximum: 5 }, uniqueness: { case_sensitive: false }
 
+  extend FriendlyId
+  friendly_id :slug_candidates, use: :slugged
+
+  def slug_candidates
+    [
+      full_code
+    ]
+  end
+
   def full_code
     project.full_code + '-' + code
   end
