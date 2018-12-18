81ad450305b0ee7b0eae820a6599af89cb90d786
James Keane <jpkeane@me.com>
2018-12-18 22:45:59 +0000

    Added Project model spec

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -21,4 +21,15 @@ RSpec.describe Project, type: :model do
   describe 'relationships' do
     it { is_expected.to belong_to :client }
   end
+
+  describe 'validations' do
+    subject do
+      FactoryBot.create(:project)
+    end
+
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:code) }
+    it { is_expected.to validate_length_of(:name).is_at_most(100) }
+    it { is_expected.to validate_length_of(:code).is_at_most(5) }
+  end
 end
