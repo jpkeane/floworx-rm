a63a1475a75d16e3b76845334b1abbad5663a0aa
James Keane <jpkeane@me.com>
2018-12-19 01:16:32 +0000

    Used specs for User remember token checks

--- spec/features/user_sign_in_sign_out.rb
+++ spec/features/user_sign_in_sign_out.rb
@@ -1,17 +1,37 @@
 require 'rails_helper'
 
-RSpec.feature 'User Sign in and Sign out', type: :feature do
-  let(:user) { FactoryBot.create(:user, password: 'testpassword', password_confirmation: 'testpassword') }
+RSpec.feature 'User Login and Logout', type: :feature do
+  let(:user) { FactoryBot.create(:user, password: 'testpassword') }
 
-  scenario 'User logs in successfully with email address' do
+  scenario 'User logs in successfully with username' do
     successful_sign_in
     expect(page).to have_content('Log in successful')
+    expect(Capybara.current_session.driver.request.cookies['remember_token']).to be_nil
   end
 
-  scenario 'User signs in unsuccessfully' do
+  scenario 'User logs in successfully with email address' do
+    successful_sign_in(email: true)
+    expect(page).to have_content('Log in successful')
+    expect(Capybara.current_session.driver.request.cookies['remember_token']).to be_nil
+  end
+
+  scenario 'User logs in successfully then visits login page' do
+    successful_sign_in
+    visit login_path
+    expect(page).to have_content('You are already logged in')
+  end
+
+  scenario 'User logs in with remembering' do
+    successful_sign_in(remember: true)
+    expect(Capybara.current_session.driver.request.cookies['remember_token']).not_to be_nil
+    successful_log_out
+    expect(Capybara.current_session.driver.request.cookies['remember_token']).to be_nil
+  end
+
+  scenario 'User logs in unsuccessfully' do
     visit root_path
     click_link 'Log in'
-    fill_in 'Email', with: user.email_address
+    fill_in 'Email address', with: user.email_address
     fill_in 'Password', with: 'notthepassword'
     click_button 'Log in'
     expect(page).to have_content('Invalid Email or password')
@@ -20,13 +40,18 @@ RSpec.feature 'User Sign in and Sign out', type: :feature do
   scenario 'User logs out successfully' do
     successful_sign_in
     successful_log_out
+    visit dashboard_path
+    expect(page).to have_content('You are not logged in')
   end
 
-  def successful_sign_in
+  private
+
+  def successful_sign_in(options = {})
     visit root_path
     click_link 'Log in'
     fill_in 'Email address', with: user.email_address
     fill_in 'Password', with: user.password
+    check('session_remember_me') if options[:remember]
     click_button 'Log in'
   end
 
