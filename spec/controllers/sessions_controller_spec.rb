f4afb97e0c05ef6ccaa67f88a507c70950bc50cf
James Keane <jpkeane@me.com>
2018-12-19 02:54:48 +0000

    Added invalid password controller spec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -11,9 +11,17 @@ RSpec.describe SessionsController, type: :controller do
       end
     end
 
-    describe 'POST #create' do
+    describe 'POST #create with valid password' do
       it 'returns http success' do
         post :create, params: { session: { email_address: user.email_address, password: user.password } }
+        expect(response).to have_http_status(:found)
+      end
+    end
+
+    describe 'POST #create with invalid password' do
+      it 'returns http success' do
+        post :create, params: { session: { email_address: user.email_address, password: 'notthepass' } }
+        expect(response).to have_http_status(:success)
       end
     end
   end
