3c7a9e42b9f2e93562d42e1ad227cac325ee425f
James Keane <jpkeane@me.com>
2018-12-19 01:30:25 +0000

    Added Remember and Forget specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,4 +20,44 @@ RSpec.describe User, type: :model do
     it { is_expected.to have_attribute :first_name }
     it { is_expected.to have_attribute :last_name }
   end
+
+  describe 'methods' do
+    it '#full_name should return the users full name' do
+      user = FactoryBot.create(:user)
+      expect(user.full_name).to eq "#{user.first_name} #{user.last_name}"
+    end
+  end
+
+  it '#remember creates new token' do
+    user = FactoryBot.create(:user)
+    5.times do
+      user.remember
+    end
+    expect(user.user_remember_tokens.count).to eq 5
+  end
+
+  it '#forget_remember_token removes a token' do
+    user = FactoryBot.create(:user)
+    user.remember
+    expect(user.user_remember_tokens.count).to eq 1
+    user.forget_remember_token(user.user_remember_tokens.first.remember_digest)
+    expect(user.user_remember_tokens.count).to eq 0
+  end
+
+  it '#forget_all_remember_tokens removes all tokens' do
+    user = FactoryBot.create(:user)
+    5.times do
+      user.remember
+    end
+    expect(user.user_remember_tokens.count).to eq 5
+    user.forget_all_remember_tokens
+    expect(user.user_remember_tokens.count).to eq 0
+  end
+
+  it '#create_password_reset_token creates password reset token' do
+    user = FactoryBot.create(:user)
+    user.create_password_reset_token
+    expect(user.password_reset_token).not_to be_blank
+    expect(user.password_reset_token_at).to be_within(2.seconds).of Time.zone.now
+  end
 end
