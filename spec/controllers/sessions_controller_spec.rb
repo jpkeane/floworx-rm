ca8574af99db2e7dea8d6afbcbea99faf9f30fbf
James Keane <jpkeane@me.com>
2018-12-19 02:17:41 +0000

    Added Session test for logged in user

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,10 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
-  describe 'GET #new' do
-    it 'returns http success' do
-      get :new
-      expect(response).to have_http_status(:success)
+  let!(:user) { FactoryBot.create(:user) }
+
+  context 'when not signed in' do
+    describe 'GET #new' do
+      it 'returns http success' do
+        get :new
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+
+  context 'when signed in' do
+    describe 'GET #new' do
+      it 'returns http redirect' do
+        request.session[:user_id] = user.id
+        get :new
+        expect(response).to have_http_status(:found)
+      end
     end
   end
 end
